b6b85672513aa97d1ab86767d337566d58e859a8
Nguyen Trung <nguyentrung0904@gmail.com>
2018-05-18 16:11:20 +0800

    Added bundle and parse Int

--- App.js
+++ App.js
@@ -10,9 +10,8 @@ export default class App extends Component<> {
     CONFIG.rootTag = props.rootTag ? props.rootTag : '1'
     CONFIG.token = props.token != null ? props.token : 'Token 0c7e79288c96b08c984cb24c31e8622171c25e14'
     CONFIG.url = props.url != null ? props.url : 'http://13.250.247.107:8003/v1/form'
-    CONFIG.formid = props.formid != null ? props.formid : 0
-    CONFIG.formtype = props.formtype != null ? props.formtype : 0
-
+    CONFIG.formid = props.formid != null ? parseInt(props.formid) : 0
+    CONFIG.formtype = props.formtype != null ? parseInt(props.formtype) : 0
   }
 
   render () {
